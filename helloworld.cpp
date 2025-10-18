aecf30e19c42e315752b96822a93f3eb58eee789
Seb3010 <sebastianpdlp@outlook.com>
2025-10-18 15:06:41 -0600

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    int x;
+    x = 5;
+
+    int y = 6;
+
+    int suma = x + y;
+
+    std::cout << suma << std::endl;
+    std::cout << y << std::endl;
+    std::cout <<x << std::endl;
+    std::cout << "Hello, World!"<< std::endl;
+    std::cout << "This is a test";
+}
